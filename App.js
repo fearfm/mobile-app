4278d4c58e0a3230b123d7faffa71cb01c8bcf01
Pim Jansen <pjansen@senet.nl>
2020-05-08 15:10:19 +0200

    Player updates with react-native-player

--- App.js
+++ App.js
@@ -8,153 +8,76 @@ import {
   View
 } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
-import { Audio } from "expo-av";
+import TrackPlayer from 'react-native-track-player';
 
-const AUDIO_STREAM_URI = 'https://stream.pimjansen.dev';
+const IMAGE_PLACEHOLDER = require("./assets/placeholder.jpg");
+// const AUDIO_STREAM_URI = 'https://stream.pimjansen.dev';
 
 export default class App extends React.Component {
+
   constructor(props) {
     super(props);
-    this.index = 0;
-    this.playbackInstance = null;
+
     this.state = {
-      artist: 'Fear.FM',
-      title: 'Be Proud and Listen Loud',
-      shouldPlay: true,
-      isPlaying: false,
-      isBuffering: false,
       isLoading: true,
-      shouldCorrectPitch: false,
-      volume: 1.0,
-      rate: 1.0,
-      useNativeControls: false,
-      throughEarpiece: false
-    };
+      isPlaying: true,
+      artist: 'Fear.FM',
+      title: 'Be Proud and Listen Loud'
+    }
+    this.player = null;
   }
 
   componentDidMount() {
-    Audio.setAudioModeAsync({
-      allowsRecordingIOS: false,
-      staysActiveInBackground: true,
-      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-      playsInSilentModeIOS: true,
-      shouldDuckAndroid: true,
-      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-      playThroughEarpieceAndroid: false
+    const { artist, title } = this.state;
+
+    TrackPlayer.setupPlayer({
+      stopWithApp: false,
+      volume: 1
+    }).then(() => {
+      var track = {
+        id: 'fearfm-stream', // Must be a string, required
+        url: 'https://stream.fear.fm', // Load media from the network
+        title,
+        artist,
+        artwork: IMAGE_PLACEHOLDER, // Load artwork from the network
+      };
+      TrackPlayer.add([track]).then(function(a) {
+        TrackPlayer.play();
+      });
     });
-    this._loadNewPlaybackInstance(true);
   }
 
-  async _loadNewPlaybackInstance(playing) {
-    if (this.playbackInstance !== null) {
-      await this.playbackInstance.unloadAsync();
-      this.playbackInstance = null;
-    }
-
-    const source = { uri: AUDIO_STREAM_URI };
-    const initialStatus = {
-      shouldPlay: playing,
-      rate: this.state.rate,
-      shouldCorrectPitch: this.state.shouldCorrectPitch,
-      volume: this.state.volume,
-    };
-
-    const { sound, status } = await Audio.Sound.createAsync(
-      source,
-      initialStatus,
-      this._onPlaybackStatusUpdate
-    );
-    this.playbackInstance = sound;
-    this._updateScreenForLoading(false);
-  }
-
-  _updateScreenForLoading(isLoading) {
-    if (isLoading) {
+  onPlayPauseclick = () => {
+    const { isPlaying } = this.state;
+    if (isPlaying) {
+      TrackPlayer.stop();
       this.setState({
-        isPlaying: false,
-        isLoading: true
+        isPlaying: false
       });
     } else {
+      TrackPlayer.play();
       this.setState({
-        isLoading: false
+        isPlaying: true
       });
     }
   }
-
-  _onPlaybackStatusUpdate = status => {
-    if (status.isLoaded) {
-      this.setState({
-        shouldPlay: status.shouldPlay,
-        isPlaying: status.isPlaying,
-        isBuffering: status.isBuffering,
-        rate: status.rate,
-        volume: status.volume,
-        shouldCorrectPitch: status.shouldCorrectPitch
-      });
-      if (status.didJustFinish && !status.isLooping) {
-        this._advanceIndex(true);
-        this._updatePlaybackInstanceForIndex(true);
-      }
-    } else {
-      if (status.error) {
-        console.log(`FATAL PLAYER ERROR: ${status.error}`);
-      }
-    }
-  };
-
-  _onLoadStart = () => {
-    console.log(`ON LOAD START`);
-  };
-
-  _onLoad = status => {
-    console.log(`ON LOAD : ${JSON.stringify(status)}`);
-  };
-
-  _onError = error => {
-    console.log(`ON ERROR : ${error}`);
-  };
-
-  async _updatePlaybackInstanceForIndex(playing) {
-    this._updateScreenForLoading(true);
-
-    this.setState({
-      videoWidth: DEVICE_WIDTH,
-      videoHeight: VIDEO_CONTAINER_HEIGHT
-    });
-
-    this._loadNewPlaybackInstance(playing);
-  }
-
-  _onPlayPausePressed = () => {
-    if (this.playbackInstance !== null) {
-      if (this.state.isPlaying) {
-        this.playbackInstance.stopAsync();
-      } else {
-        this.playbackInstance.playAsync();
-      }
-    }
-  };
-
-  _onUseNativeControlsPressed = () => {
-    this.setState({ useNativeControls: !this.state.useNativeControls });
-  };
 
   render() {
-    const { isPlaying } = this.state;
+    const { artist, title, isPlaying } = this.state;
     return (
       <SafeAreaView style={styles.container}>
         <View style={{ alignItems: "center" }}>
           <View style={styles.coverContainer}>
-            <Image source={require("./assets/placeholder.jpg")} style={styles.cover}></Image>
+            <Image source={IMAGE_PLACEHOLDER} style={styles.cover}></Image>
           </View>
           <View style={{ alignItems: "center", marginTop: 32 }}>
-            <Text style={[styles.textDark, { fontSize: 20, fontWeight: "500" }]}>{this.state.artist}</Text>
-            <Text style={[styles.text, { fontSize: 16, marginTop: 8 }]}>{this.state.title}</Text>
+            <Text style={[styles.textDark, { fontSize: 20, fontWeight: "500" }]}>{artist}</Text>
+            <Text style={[styles.text, { fontSize: 16, marginTop: 8 }]}>{title}</Text>
           </View>
         </View>
         <View style={styles.footer}>
           <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginTop: 50 }}>
-            <TouchableOpacity style={styles.playButtonContainer} onPress={this._onPlayPausePressed}>
+            <TouchableOpacity style={styles.playButtonContainer} onPress={this.onPlayPauseclick}>
               <FontAwesome5
                 name={isPlaying ? 'pause' : 'play'}
                 size={32}
